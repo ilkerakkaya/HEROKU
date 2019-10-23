0333223e94e4e989e69272b2781d9d059ac11dbf
ilkerakkaya <ilkerakkaya@gmail.com>
2019-10-23 22:58:20 +0200

    update

--- app.js
+++ app.js
@@ -138,11 +138,8 @@ app.get("/about", function(req, res) {
 });
 
 
-let port = process.env.PORT;
+let port = process.env.PORT || 3000;
 
-if (port==null || port== ""){
-  port = 3000;
-}
 
 app.listen(port, function() {
   console.log("Server has started succesfully");
